d2a4f6c5aa1cef28488749926b7732d87e738e41
이다은 <daeun.lee@coinplug.com>
2018-03-15 16:37:13 +0900

    first commit ( nomadcoder react class )

---
+++ src/App.js
@@ -0,0 +1,65 @@
+import React, { Component } from 'react';
+import './App.css';
+import Movie from './Movie.js';
+
+// smart component
+// state 가 존재한다 ==> LifeCycle이 존재한다
+class App extends Component {
+  // ----- LifeCycle ----
+  // Render : componentWillMount() -> render() -> componentDidMount()
+  // Update : componentWillReceiveProps() -> shouldComponentUpdate() -> componentWillUpdate() -> render() -> componentDidMount()
+
+  /**
+   * render() 후 불려지는 method
+   */
+  componentDidMount() {
+    this._getMovies();
+  }
+  /**
+   * async function 으로 지정
+   * callApi() 가 끝날때까지 await (기다림)
+   *
+   * await 은 async fuction 에서만 사용가능
+   */
+  _getMovies = async () => {
+    const movies = await this._callApi()
+    this.setState({
+      movies
+    })
+  }
+
+  _callApi = () => {
+    return fetch('https://yts.am/api/v2/list_movies.json?sort_by=download_count')
+    .then(response => response.json())
+    .then(json => json.data.movies)
+    .catch(err => console.log(err))
+  }
+
+  /**
+   * override 한 method 가 아닌
+   * 개발자가 직접 구현한 method는 명명시 앞에 '_' 를 추가한다.
+   */
+  _renderMovies = () => {
+    const movies = this.state.movies.map(movie => {
+      return <Movie
+      title={movie.title_english}
+      poster={movie.medium_cover_image}
+      key={movie.id}
+      genres={movie.genres}
+      synopsis={movie.synopsis} />
+    })
+    return movies
+  }
+
+  render() {
+    const movies = this.state;
+    return (
+      // data 확인 후 call method
+      <div className={movies ? "App" : "App--loading"} >
+        {movies ? this._renderMovies() : 'Loading'}
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/Movie.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import LinesEllipsis from 'react-lines-ellipsis'
+import './Movie.css';
+
+// dump component
+// state 가 없다 ==> LifeCycle 이 없다
+function Movie({title, poster, genres, synopsis}) {
+  return (
+    <div className="Movie">
+      <div className="Movie__Colums">
+        <MoviePoster poster={poster} alt={title} />
+      </div>
+      <div className="Movie__Colums">
+        <h1>{title}</h1>
+        <div className="Movie__Genres">
+          {genres.map((genre, index) =>
+            <MovieGenre genre={genre} key={index} />)
+          }
+        </div>
+        <p className="Movie__Synopsis">
+          <LinesEllipsis
+            text={synopsis}
+            maxLine='3'
+            ellipsis=' ...'
+            trimRight
+            basedOn='letters'
+            />
+        </p>
+      </div>
+    </div>
+  )
+}
+
+function MoviePoster({poster, alt}) {
+  return (
+    <img src={poster} alt={alt} title={alt} className="Movie__Poster" />
+  )
+}
+
+function MovieGenre({genre}) {
+  return (
+    <span className="Movie__Genre">{genre}</span>
+  )
+}
+
+Movie.propTypes = {
+  title: PropTypes.string.isRequired,
+  poster: PropTypes.string.isRequired,
+  genres: PropTypes.array.isRequired,
+  synopsis: PropTypes.string.isRequired
+}
+
+MoviePoster.propTypes = {
+  poster: PropTypes.string,
+  alt: PropTypes.string.isRequired
+}
+
+MovieGenre.propTypes = {
+  genre: PropTypes.string.isRequired
+}
+
+
+export default Movie;
